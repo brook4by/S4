4e351718f911e16b09b2d3c585debaed93b2c274
brook4by <brook4by@gmail.com>
2021-12-22 10:23:19 +0300

    init

---
+++ E3/PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,38 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PlatformRepo(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        void IPlatformRepo.CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _dbContext.Platforms.Add(platform);
+        }
+
+        Platform IPlatformRepo.GetPlatformById(int id)
+        {
+            return _dbContext.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        IEnumerable<Platform> IPlatformRepo.GeAlltPlatforms()
+        {
+            return _dbContext.Platforms.ToList();
+        }
+
+        bool IPlatformRepo.SaveChanges()
+        {
+            return (_dbContext.SaveChanges() >=0);
+        }
+    }
+}
