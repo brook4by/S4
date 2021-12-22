6ec00917b798a373861006f41800710e15eaf10f
brook4by <brook4by@gmail.com>
2021-12-22 23:25:31 +0300

    fix

---
+++ E3/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private IPlatformRepo _repository;
+        private IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("---> Getting Platforms");
+
+            IEnumerable<Models.Platform> platforms = _repository.GeAlltPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById([FromRoute]int id)
+        {
+            var platform =_repository.GetPlatformById(id);
+            if (platform!=null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+            }
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platform)
+        {
+            var platformModel = _mapper.Map<Platform>(platform);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { platformReadDto.Id }, platformReadDto);
+        }
+
+    }
+}
