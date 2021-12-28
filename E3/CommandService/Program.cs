8ff25a0a3096ade93d21ea4dce9f0298902f313f
VT\vladislav.zinkovich <vvzinkovich@vebtech.by>
2021-12-28 16:01:34 +0300

    28

--- E3/CommandService/Program.cs
+++ E3/CommandService/Program.cs
@@ -1,18 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-using PlatformService.Data;
-using PlatformService.SyncDataServices.Http;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<PlatformService.Data.AppDbContext>(opt =>
-opt.UseInMemoryDatabase("InMem"));
-
-builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -31,7 +21,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-PrepDb.PrepPopulation(app);
 
 app.Run();
