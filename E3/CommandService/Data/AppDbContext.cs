620612ab69ee9747fda1a8980f1770c53294c0a1
VT\vladislav.zinkovich <vvzinkovich@vebtech.by>
2022-02-11 16:47:45 +0300

    11_02

--- E3/CommandService/Data/AppDbContext.cs
+++ E3/CommandService/Data/AppDbContext.cs
@@ -15,6 +15,17 @@ namespace CommandsService.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(p => p.Platform)
+                .HasForeignKey(p => p.PlatformId);
+
+            modelBuilder.Entity<Command>()
+                .HasOne(p => p.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(p => p.PlatformId);
+
+
             base.OnModelCreating(modelBuilder);
         }
     }
